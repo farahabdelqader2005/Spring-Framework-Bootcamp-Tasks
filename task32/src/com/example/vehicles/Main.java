9318d74b4093fbf707d5bec8219dc6adaa52905d
farahabdelqader2005 <bdalqadrfrh6@gmail.com>
2026-07-18 03:59:29 +0300

    Add Task 32 project

---
+++ task32/src/com/example/vehicles/Main.java
@@ -0,0 +1,34 @@
+package com.example.vehicles;
+
+import org.springframework.context.support
+        .ClassPathXmlApplicationContext;
+public class Main {
+
+    public static void main(String[] args) {
+
+        try (
+                ClassPathXmlApplicationContext container =
+                        new ClassPathXmlApplicationContext(
+                                "applicationContext.xml"
+                        )
+        )
+        {
+
+            Vehicle car =
+                    container.getBean(
+                            "car",
+                            Vehicle.class
+                    );
+
+            Vehicle plane =
+                    container.getBean(
+                            "plane",
+                            Vehicle.class
+                    );
+
+            car.saveData();
+
+            plane.saveData();
+        }
+    }
+}
