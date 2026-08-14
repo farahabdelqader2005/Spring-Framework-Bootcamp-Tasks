b310cbfc33004a14f284eca065bbfbbd897c5f1e
farahabdelqader2005 <bdalqadrfrh6@gmail.com>
2026-08-14 12:48:25 +0300

    Add Task 3.2

---
+++ Task3.2/src/com/example/beanlifecycle/DatabaseOperations.java
@@ -0,0 +1,12 @@
+package com.example.beanlifecycle;
+
+public class DatabaseOperations {
+
+    public void connectToDatabase() {
+        System.out.println("connection to database established");
+    }
+
+    public void disconnectFromDatabase() {
+        System.out.println("connection to database closed");
+    }
+}
